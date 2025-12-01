83673861420c79d11bc9c699f101ce28384ee04b
sandraclarkdev <58484559+sandraclarkdev@users.noreply.github.com>
2025-12-01 15:33:50 -0500

    base structure issues resolved

--- src/folio/BaseStructure/BaseStructureView.tsx
+++ src/folio/BaseStructure/BaseStructureView.tsx
@@ -7,10 +7,12 @@ export function BaseStructureView(navObj) {
     const navigation = transformNavigation(navObj.navigation)
     return (
         <>
-            <Heading headingLevel={2}>Prototype Example</Heading>
+            <Heading headingLevel={2}>Base Structure Example</Heading>
             <Text>This example renders a structurally semantic component along with the basic aria requirements. You can
-                read the accompanying article <Link href="">Creating the First Layer - Implementing Basic Structure</Link> on Dev.to</Text>
-            
+                read the accompanying article <Link
+                    href="https://dev.to/shaynaproductions/first-layer-implementing-a-base-structure-492o"
+                    openInNewTab={true}>Creating the First Layer - Implementing Basic Structure</Link> on Dev.to</Text>
+
             <Navigation
                 id="prototype"
                 label="Basic Structure"
