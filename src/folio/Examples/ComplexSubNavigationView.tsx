f8661a9ae983d73e734d7e76a8494d4ca6a7bb18
sandraclarkdev <58484559+sandraclarkdev@users.noreply.github.com>
2025-12-21 06:34:21 -0500

    Examples Updated

--- src/folio/Examples/ComplexSubNavigationView.tsx
+++ src/folio/Examples/ComplexSubNavigationView.tsx
@@ -1,27 +1,46 @@
 "use client";
-import {Box, Button, Heading, Navigation, NavigationLinkProps, Text, transformNavigation,} from "@/ui/components";
+import {
+  Box,
+  Button,
+  Heading,
+  Navigation,
+  NavigationLinkProps,
+  Text,
+  transformNavigation,
+} from "@/ui/components";
 
-export function ComplexSubNavigationView(navObject: { navigation: NavigationLinkProps[]; }) {
-    const navigation = transformNavigation(navObject.navigation);
+export function ComplexSubNavigationView(navObject: {
+  navigation: NavigationLinkProps[];
+}) {
+  const navigation = transformNavigation(navObject.navigation);
 
-    return (
-        <>
-            <Heading headingLevel={3}>Complex SubNavigation</Heading>
-            <Text>An example of multiple nested subnavigation components. Focusable Front and End buttons help define
-                keyboard traps.</Text>
-            <Text><strong>Note: </strong>This example now implements keyboard handling
-                between sublists. Use the down and up arrow keys to move between a button and its controled list. Use the up arrow key to move up through collapsed or expanded lists.</Text>
-            <Box cx="example complex">
-                <Button id="front">Focusable Front</Button>
-                <Navigation
-                    id="complex-navigation"
-                    label="Complex Sub Navigation Demo"
-                    orientation={"horizontal"}
-                >
-                    {navigation}
-                </Navigation>
-                <Button id="end">Focusable End</Button>
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Heading headingLevel={3}>Complex SubNavigation</Heading>
+      <Text>
+        An example of multiple nested subnavigation components. Focusable Front
+        and End buttons help define keyboard traps.
+      </Text>
+      <Text>
+        <strong>Note: </strong>This example implements keyboard handling between
+        sublists using the tab keys to move in, out and through the navigation
+        items. Tab and arrow-down follow many of the same requirements, as does
+        Shift-Tab follow the arrow-up functionality. Use the tab key at the end
+        to move out of the component and then shift-key to move back in. Use the
+        Tab key to move into the component at the beginning and then Shift-Tab
+        to move back out.
+      </Text>
+      <Box cx="example complex">
+        <Button id="front">Focusable Front</Button>
+        <Navigation
+          id="complex-navigation"
+          label="Complex Sub Navigation Demo"
+          orientation={"horizontal"}
+        >
+          {navigation}
+        </Navigation>
+        <Button id="end">Focusable End</Button>
+      </Box>
+    </>
+  );
 }
